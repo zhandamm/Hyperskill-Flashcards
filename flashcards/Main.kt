06fce827fdb8f578a5b5dd73e0c5a9d8b4353384
zhandamm <sai.eduself@gmail.com>
2023-05-17 17:13:48 +0300

    stage4

--- flashcards/Main.kt
+++ flashcards/Main.kt
@@ -5,41 +5,54 @@ fun main() {
 
     card.createCard()
     card.answerForQuest()
-    /*    println("Input the number of cards:")
-        val numberOfCards = readln().trim().toInt()
 
-        val answer = readln().trim()
-        if (definition == answer) {
-            println("Your answer is right!")
-        } else println("Your answer is wrong...")*/
 
 }
 
 
-class Card(private val question: String = "", private val definition: String = "") {
-    private val cards = mutableListOf<Card>()
+class Card {
+    /*    private val question: String = ""
+        private val definition: String = ""*/
+    private val cards = mutableMapOf<String, String>()
 
     fun createCard() {
         println("Input the number of cards:")
         val numberOfCards = readln().trim().toInt()
-        for (i in 1..numberOfCards) {
-            println("Card #${i}:")
-            val questionInput = readln()
-            println("The definition for card #${i}:")
-            val definition = readln().trim()
-            val card = Card(questionInput, definition)
-            cards.add(card)
+        var i = 0
+        while (i != numberOfCards) {
+
+            println("Card #${i + 1}:")
+            var questionInput = readln().trim()
+            while (cards.containsKey(questionInput)) {
+                println("The term \"$questionInput\" already exists. Try again:")
+                questionInput = readln().trim()
+
+            }
+            println("The definition for card #${i + 1}:")
+            var definitionInput = readln().trim()
+            while (cards.containsValue(definitionInput)) {
+                println("The definition \"$definitionInput\" already exists. Try again:")
+                definitionInput = readln().trim()
+            }
+            cards[questionInput] = definitionInput
+            i++
         }
+
     }
+
 
     fun answerForQuest() {
         for (i in cards) {
-            println("Print the definition of \"${i.question}\":")
+            println("Print the definition of \"${i.key}\":")
             val answer = readln()
-            if (i.definition == answer) {
+            if (i.value == answer) {
                 println("Correct!")
-            } else{
-                println("Wrong. The right answer is \"${i.definition}\".")
+            } else if (cards.containsValue(answer)) {
+                val keysWithMatchingValue = cards.entries.filter { it.value == answer }.map { it.key }
+                val keyString = keysWithMatchingValue.joinToString { it }
+                println("Wrong. The right answer is \"${i.value}\", but your definition is correct for \"${keyString}\".")
+            } else {
+                println("Wrong. The right answer is \"${i.value}\".")
             }
         }
     }
